390daf923beb75ab9290d852d4e2678cbf1924cf
salwa-59 <154442739+salwa-59@users.noreply.github.com>
2025-07-06 15:11:43 +0300

    Delete Booking/booking directory

--- Booking/booking/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-  return isLoggedIn ? <Outlet /> : <Navigate to="/" replace />;
-};
-
-export default PrivateRoute;
